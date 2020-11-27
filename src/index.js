8cf7a1794ae133ff750a22c8c549dfc867245b59
Ángel Pérez <angelpxrz@gmail.com>
2020-11-27 12:38:06 +0100

    Add timestamp to location messages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
 
-const { generateMessage } = require('./utils/messages')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 app = express()
 const server = http.createServer(app)
@@ -33,7 +33,7 @@ io.on('connection', (socket) => {
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
